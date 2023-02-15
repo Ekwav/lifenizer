5012f93454fbc374af989cb70279c31558491944
Äkwav <ekwav@coflnet.com>
2023-02-15 12:42:59 +0100

    update core and cli

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ImportComponent } from './lifenizer/import/import.component';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, {})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
